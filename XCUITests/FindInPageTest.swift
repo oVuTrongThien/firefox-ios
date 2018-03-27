15f39d3d26d583b241c1189b2dfa54ba4380e265
No Jun Park <npark@mozilla.com>
2018-03-27 11:10:52 -0400

    add testFindInLargeDoc

--- XCUITests/FindInPageTest.swift
+++ XCUITests/FindInPageTest.swift
@@ -17,14 +17,11 @@ class FindInPageTests: BaseTestCase {
         userState.url = "http://localhost:6571/find-in-page-test.html"
         openFindInPageFromMenu()
 
-      //  loadWebPage("http://www.angelfire.com/falcon/flamingchickens/abcd.html", waitForLoadToFinish: false)
-
         // Enter some text to start finding
         app.textFields[""].typeText("Book")
 
-        // @TODO add these in once code lands
-//        waitforExistence(app.staticTexts["1/500+"])
-//        XCTAssertTrue(app.staticTexts["1/500+"].exists)
+        waitforExistence(app.staticTexts["1/500+"])
+        XCTAssertTrue(app.staticTexts["1/500+"].exists)
     }
 
     func testFindFromMenu() {
